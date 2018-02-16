1ec8d06ff531acbebd10511d20eb419ebd0e0c26
Joel De Leon <joel.deleon95@gmail.com>
2018-02-16 09:21:03 -0500

    true commit

--- backgroundApp.js
+++ backgroundApp.js
@@ -23,7 +23,7 @@ function draw() {
   // We are going to draw a polygon out of the wave points
     
    stroke(0,255,0); 
-    fill(0);
+    noFill();
     strokeWeight(1);
     
     
@@ -88,32 +88,32 @@ beginShape();
      // image(img, 0, 0, windowWidth, newHeight);
     
  
-     stroke(0,255,0); 
-    fill(0);
-    strokeWeight(5);
-    
-    
-  beginShape(); 
-  
-  var xoff = 0;       // Option #1: 2D Noise
-  // float xoff = yoff; // Option #2: 1D Noise
-  
-  // Iterate over horizontal pixels
-  for (var x = 0; x <= width; x += 10) {
-    // Calculate a y value according to noise, map to 
-    var y = map(noise(xoff, yoff), 0, 1, 300,500); // Option #1: 2D Noise
-    // float y = map(noise(xoff), 0, 1, 200,300);    // Option #2: 1D Noise
-    
-    // Set the vertex
-    vertex(x, y); 
-    // Increment x dimension for noise
-    xoff += 0.05;
-  }
-  // increment y dimension for noise
-  yoff += 0.03;
-  vertex(width, height);
-  vertex(0, height);
-  endShape(CLOSE);
+//     stroke(0,255,0); 
+//    noFill();
+//    strokeWeight(5);
+//    
+//    
+//  beginShape(); 
+//  
+//  var xoff = 0;       // Option #1: 2D Noise
+//  // float xoff = yoff; // Option #2: 1D Noise
+//  
+//  // Iterate over horizontal pixels
+//  for (var x = 0; x <= width; x += 10) {
+//    // Calculate a y value according to noise, map to 
+//    var y = map(noise(xoff, yoff), 0, 1, 300,500); // Option #1: 2D Noise
+//    // float y = map(noise(xoff), 0, 1, 200,300);    // Option #2: 1D Noise
+//    
+//    // Set the vertex
+//    vertex(x, y); 
+//    // Increment x dimension for noise
+//    xoff += 0.05;
+//  }
+//  // increment y dimension for noise
+//  yoff += 0.03;
+//  vertex(width, height);
+//  vertex(0, height);
+//  endShape(CLOSE);
     
     
    stroke(0,255,0); 
